d5dd03df93390b268a5351fdbb692a9f6412573d
Leandro Lima <lleandrorj@gmail.com>
2024-03-27 18:39:32 -0300

    feat: :sparkles: Instanciando a classe Nokia e imprimindo no console os testes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new Nokia(numero: "(11)99999-9999", modelo: "Nokia 3210", imei: "123456789", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
