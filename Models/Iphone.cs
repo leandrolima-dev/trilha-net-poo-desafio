947fb0458266078c07469cc46e6e0da2a3f6757c
Leandro Lima <lleandrorj@gmail.com>
2024-03-27 18:50:30 -0300

    feat: :sparkles: Instanciando a classe Iphone e imprimindo no console os testes

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,17 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            Console.WriteLine($"Modelo: 📱{modelo}");
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando aplicativo {nomeApp} no Iphone ✅");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
-Console.WriteLine("Smartphone Nokia");
+Console.WriteLine("Smartphone - Nokia");
 Nokia nokia = new Nokia(numero: "(11)99999-9999", modelo: "Nokia 3210", imei: "123456789", memoria: 32);
 nokia.Ligar();
-nokia.InstalarAplicativo("WhatsApp");
+nokia.InstalarAplicativo("- WhatsApp,");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone - Iphone");
+Iphone iphone = new Iphone(numero: "(21)99999-0000", modelo: "Iphone 13", imei: "987654321", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("- Telegram,");
