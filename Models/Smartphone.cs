1df0ddcbaa6bed3a0d740c02a43976d22ed4d5ba
Leandro Lima <lleandrorj@gmail.com>
2024-03-27 15:53:37 -0300

    feat: :sparkles: Resolvendo os TODOs da classe Smartphone, conforme as regras do desafio

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,16 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        private string _modelo;
+        private string _imei;
+        private int _memoria;
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
-            Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Numero = numero;
+            _modelo = modelo;
+            _imei = imei;
+            _memoria = memoria;
         }
 
         public void Ligar()
