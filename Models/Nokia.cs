75b39a6f7cfd9b4c587f0533a930750adb7ff967
Leandro Lima <lleandrorj@gmail.com>
2024-03-27 18:49:55 -0300

    style: :lipstick: add emojis no texto de saída do console

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -5,13 +5,13 @@ namespace DesafioPOO.Models
     {
         public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
         {
-            Console.WriteLine($"Modelo: {modelo}");
+            Console.WriteLine($"Modelo: 📱{modelo}");
         }
 
         // TODO: Sobrescrever o método "InstalarAplicativo"
         public override void InstalarAplicativo(string nomeApp)
         {
-            Console.WriteLine($"Instalando aplicativo {nomeApp} no Nokia");
+            Console.WriteLine($"Instalando aplicativo {nomeApp} no Nokia ✅");
         }
     }
 }
